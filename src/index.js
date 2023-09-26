74d6e4b50b4c98669c2f1de16a7fd5915b511150
Largopie <106071687+Largopie@users.noreply.github.com>
2023-09-26 20:12:53 +0900

    refactor: if else 부분 switch로 변경하여 가독성 좋게 수정

--- src/index.js
+++ src/index.js
@@ -8,12 +8,15 @@ const number = document.getElementById("number");
 number.innerText = 0;
 
 const countModifier = (count = 0, action) => {
-  if(action.type === "ADD") {
-    return count + 1;
-  } else if(action.type === "MINUS") {
-    return count - 1;
-  }else {
-    return count;
+  switch (action.type) {
+    case "ADD":
+      return count + 1;
+
+    case "MINUS":
+      return count - 1;
+      
+    default:
+      return count;
   }
 };
 
