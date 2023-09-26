ddbe24bbdacd497f7a0b735c65e4b089e286ccbd
Largopie <106071687+Largopie@users.noreply.github.com>
2023-09-26 16:36:38 +0900

    feat: Redux의 전체적인 작동 방식

--- src/index.js
+++ src/index.js
@@ -7,13 +7,21 @@ const number = document.getElementById("number");
 
 const countModifier = (count = 0, action) => {
   if(action.type === "ADD") {
-    console.log("they are telling me to add one");
+    return count + 1;
+  } else if(action.type === "MINUS") {
+    return count - 1;
+  }else {
+    return count;
   }
-  return count;
 };
 
 const countStore = createStore(countModifier);
 
 countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "MINUS"});
 
 console.log(countStore.getState());
