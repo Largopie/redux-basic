99f0ce5a72a7e660b81de34cde8e4d5a229bc81a
Largopie <106071687+Largopie@users.noreply.github.com>
2023-09-26 16:35:37 +0900

    feat: action 작동 방식

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Vanilla Redux와 React-Redux 학습 저장소
 
 ## Redux
 
-- store: 데이터를 저장하는 곳
-- createStore: reducer 요구
-- reducer는 data를 modify 해주는 함수로 reducer가 return하는 것은 application에 있는 data가 됨
+- [] store: 데이터를 저장하는 곳
+- [] createStore: reducer 요구
+- [] reducer는 data를 modify 해주는 함수로 reducer가 return하는 것은 application에 있는 data가 됨
+- [] action: Redux에서 function을 부를 때 쓰는 두 번째 parameter 또는 argument로 reducer와 소통하기 위한 방법
--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.getElementById("number");
 
-const countModifier = (state = 0) => {
-  return state;
+const countModifier = (count = 0, action) => {
+  if(action.type === "ADD") {
+    console.log("they are telling me to add one");
+  }
+  return count;
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({type: "ADD"});
 
 console.log(countStore.getState());
