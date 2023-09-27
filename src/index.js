cfae836a4ce646ea03be5a074611e045c72c2ce7
Largopie <106071687+Largopie@users.noreply.github.com>
2023-09-28 00:26:24 +0900

    feat: DEL 버튼 클릭 시 DELETE_TODO가 동작하도록 연결

--- src/index.js
+++ src/index.js
@@ -24,13 +24,22 @@ const store= createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
+const deleteToDo = (e) => {
+  const id = e.target.parentNode.id;
+  store.dispatch({ type: DELETE_TODO, id });
+}
+
 const paintToDos = () => {
   const toDos = store.getState();
   ul.innerHTML = "";
   toDos.forEach(toDo => {
     const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", deleteToDo);
     li.id = toDo.id;
     li.innerText = toDo.text;
+    li.appendChild(btn);
     ul.appendChild(li);
   })
 }
