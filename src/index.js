22ded4e01da55a427e00258247fafb5bdb541977
Largopie <106071687+Largopie@users.noreply.github.com>
2023-09-28 00:18:39 +0900

    feat: 구현한 add todo 화면상 출력

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { createStore } from 'redux';
 
 const form = document.querySelector("form");
 const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
@@ -9,7 +10,7 @@ const DELETE_TODO = "DELETE_TODO";
 const reducer = (state = [], action) => {
   switch(action.type) {
     case ADD_TODO:
-      return [...state, { text:action.text, id: Date.now() }];
+      return [{ text:action.text, id: Date.now() }, ...state];
 
     case DELETE_TODO:
       return [];
@@ -21,13 +22,30 @@ const reducer = (state = [], action) => {
 
 const store= createStore(reducer);
 
+store.subscribe(() => console.log(store.getState()));
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach(toDo => {
+    const li = document.createElement("li");
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    ul.appendChild(li);
+  })
+}
+store.subscribe(paintToDos);
+
+const addToDo = (text) => {
+  store.dispatch({ type: ADD_TODO, text });
+};
+
 const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  addToDo(toDo);
 };
 
-store.subscribe(() => console.log(store.getState()));
 
 form.addEventListener("submit", onSubmit);
