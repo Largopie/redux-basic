c0754cd3f7a241791fac91b6beabd24af6e6b7a9
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-02 01:00:57 +0900

    feat: Detail Page 구현

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { actionCreators } from '../store';
+import { Link } from 'react-router-dom';
 
-const ToDo = ({ text, onBtnClick }) => {
+const ToDo = ({ text, id, onBtnClick }) => {
   return (
     <li>
-      {text}
+      <Link to={`${id}`}>{text}</Link>
       <button onClick={onBtnClick}>DEL</button>
     </li>
   );
@@ -13,8 +14,8 @@ const ToDo = ({ text, onBtnClick }) => {
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    onBtnClick:() =>  dispatch(actionCreators.deleteToDo(ownProps.id))
-  }
-}
+    onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id)),
+  };
+};
 
-export default connect(null, mapDispatchToProps) (ToDo);
+export default connect(null, mapDispatchToProps)(ToDo);
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,20 @@
-export default () => "Detail";
+import React from 'react';
+import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+function Detail({ toDos }) {
+  const { id } = useParams();
+  const {text} = toDos.find((toDo) => toDo.id === parseInt(id));
+  return (
+    <>
+      <h1>{text}</h1>
+      <h5>Created at: {id}</h5>
+    </>
+  );
+}
+
+function mapStateToProps(state, ownProps) {
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Detail);
